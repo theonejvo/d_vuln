74407b5cf8b5516794137a1a7687ae8a7d365136
Brutie <125909656+astabrutie8@users.noreply.github.com>
2025-12-15 01:29:59 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/kavahub/main.go
@@ -0,0 +1,23 @@
+// cmd/kavahub/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "kavahub/internal/kavahub"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := kavahub.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
